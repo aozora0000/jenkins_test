2ee8fac945d4be62537127986056de9db589b7ee
kohei kinoshita <aozora0000@gmail.com>
2015-01-14 15:42:05 +0900

    fixed jenkins.yml

--- tests/JenkinsTest/RedisTest.php
+++ tests/JenkinsTest/RedisTest.php
@@ -3,9 +3,8 @@ class RedisTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
         try {
             $this->redis = new Redis;
-            var_dump($_SERVER);
-            $host = (isset($_ENV['REDIS_PORT_6379_TCP_ADDR'])) ? $_ENV['REDIS_PORT_6379_TCP_ADDR'] : "127.0.0.1";
-            $port = (isset($_ENV['REDIS_PORT_6379_TCP_PORT'])) ? $_ENV['REDIS_PORT_6379_TCP_PORT'] : "6379";
+            $host = (isset($_SERVER['REDIS_PORT_6379_TCP_ADDR'])) ? $_SERVER['REDIS_PORT_6379_TCP_ADDR'] : "127.0.0.1";
+            $port = (isset($_SERVER['REDIS_PORT_6379_TCP_PORT'])) ? $_SERVER['REDIS_PORT_6379_TCP_PORT'] : "6379";
             $this->redis->connect($host,$port);
         } catch (RedisException $e) {
             $this->fail("Redisに接続出来ませんでした。");
