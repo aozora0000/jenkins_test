ba772fca01748a53a8cf6b7a91fc63f9b2c4fa5a
kohei kinoshita <aozora0000@gmail.com>
2015-01-14 15:26:56 +0900

    add links:redis && redistest

--- src/Jenkins/Jenkins.php
+++ src/Jenkins/Jenkins.php
@@ -1,11 +1,11 @@
 <?php
 namespace Jenkins;
 class Jenkins {
-	public function test1($integer) {
-		return is_integer($integer);
-	}
+    public function test1($integer) {
+        return is_integer($integer);
+    }
 
-	public function test2($string) {
-		return is_string($string);
-	}
+    public function test2($string) {
+        return is_string($string);
+    }
 }
---
+++ tests/JenkinsTest/RedisTest.php
@@ -0,0 +1,18 @@
+<?php
+class RedisTest extends PHPUnit_Framework_TestCase {
+    public function setUp() {
+        try {
+            $this->redis = new Redis;
+            $host = (isset($_ENV['REDIS_PORT_6379_TCP_ADDR'])) ? $_ENV['REDIS_PORT_6379_TCP_ADDR'] : "127.0.0.1";
+            $port = (isset($_ENV['REDIS_PORT_6379_TCP_PORT'])) ? $_ENV['REDIS_PORT_6379_TCP_PORT'] : "6379";
+            $this->redis->connect($host,$port);
+        } catch (Exception $e) {
+            $this->fail("Redisに接続出来ませんでした。");
+        }
+    }
+
+    public function testGetTime() {
+        $time = $this->redis->time();
+        $this->assertContainsOnly("string",$time);
+    }
+}
