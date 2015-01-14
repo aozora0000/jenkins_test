9fb964980e0a69c6fcc2e67ff3c7405daa89edfa
kohei kinoshita <aozora0000@gmail.com>
2015-01-14 15:40:58 +0900

    fixed jenkins.yml

--- tests/JenkinsTest/RedisTest.php
+++ tests/JenkinsTest/RedisTest.php
@@ -3,7 +3,7 @@ class RedisTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
         try {
             $this->redis = new Redis;
-            var_dump($_ENV);
+            var_dump($_SERVER);
             $host = (isset($_ENV['REDIS_PORT_6379_TCP_ADDR'])) ? $_ENV['REDIS_PORT_6379_TCP_ADDR'] : "127.0.0.1";
             $port = (isset($_ENV['REDIS_PORT_6379_TCP_PORT'])) ? $_ENV['REDIS_PORT_6379_TCP_PORT'] : "6379";
             $this->redis->connect($host,$port);
